3fed2fea5085cad3a6c41d6db7bc333185b64e1a
Hunaid2000 <hunaid2000@gmail.com>
2022-11-25 20:15:16 +0500

    Implemented all messages notifications

--- app/src/main/java/com/ass2/i192008_i192043/User.java
+++ app/src/main/java/com/ass2/i192008_i192043/User.java
@@ -18,7 +18,6 @@ public class User {
     String gender;
     String status;
     String lastSeen;
-    String playerid;
     Bitmap userImg;
 
     public Bitmap getUserImg() {
@@ -148,13 +147,5 @@ public class User {
         });
         thread.start();
 
-    }
-
-    public String getPlayerid() {
-        return playerid;
-    }
-
-    public void setPlayerid(String playerid) {
-        this.playerid = playerid;
     }
 }
